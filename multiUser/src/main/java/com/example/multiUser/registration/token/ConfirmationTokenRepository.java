f1a1f2ceaa1abf91ee7b8b8ea107ffbc74be0dc2
pkOO5 <mettupranaykumarreddy@gmail.com>
2023-11-22 16:06:53 -0600

    first commit

--- multiUser/src/main/java/com/example/multiUser/registration/token/ConfirmationTokenRepository.java
+++ multiUser/src/main/java/com/example/multiUser/registration/token/ConfirmationTokenRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.swing.text.html.Option;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
